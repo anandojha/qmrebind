50e3c7bb685cd5ba23958098bcef831cab9d4d9c
Lane Votapka <lvotapka100@gmail.com>
2023-07-13 15:15:46 -0600

    Fixed some test inconsistencies

--- qmrebind/tests/test_extra.py
+++ qmrebind/tests/test_extra.py
@@ -8,8 +8,8 @@ from .utils import get_data_filename
 import qmrebind.extra as extra
 
 def test_get_system_charge():
-    forcefield_file = get_data_filename("hostguest_solvent2.parm7")  
-    pdb_file = get_data_filename("hostguest_solvent2.pdb")  
+    forcefield_file = get_data_filename("hostguest_no_solvent2.parm7")  
+    pdb_file = get_data_filename("hostguest_no_solvent2.pdb")  
     ret = extra.get_system_charge(
-        forcefield_file=forcefield_file, input_pdb = pdb_file)
+        forcefield_file=forcefield_file, input_pdb=pdb_file)
     assert round(ret) == 0
