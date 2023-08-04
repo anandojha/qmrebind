425d8fe3d4ac4cac1d9afb6e3ff5287cae9e40df
Lane Votapka <lvotapka100@gmail.com>
2023-08-04 10:37:34 -0600

    removed dependencies for RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ copyright = ("2022, Anupam Anand Ojha. Project structure based on the "
 author = 'Anupam Anand Ojha'
 
 # The short X.Y version
-version = ''
+version = '0.4.0'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
